46b8cfa62a675ec74406e3d3af26b75d394d38cd
navyasreemaniginti <127187994+navyasreemaniginti@users.noreply.github.com>
2023-03-18 15:42:49 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const react = require("react");
+
+function Footer(){
+	
+	const currYear = new Date().getFullYear();
+	return (
+
+		<footer>
+			<p>Copyright @ {currYear}</p>
+		
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div class="note">
+      <h1>React.js BootCamp</h1>
+      <p>
+        We learnt the basics of Web devlopment,JavaScript,React.js,JSX &
+        ReactDOM and ctreated a project at the very end of the bootcamp.
+      </p>
+    </div>
+  );
+}
+export default Notes;
